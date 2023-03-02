0f02ea9eab76f57fe3e4557e8452411b00bbdb40
Ross Pierson <rlp123@miami.edu>
2023-03-02 10:35:11 -0500

    Initial commit

---
+++ app/src/main/java/edu/miami/cs/ross_pierson/mondrianmapmaker/WelcomeActivity.java
@@ -0,0 +1,19 @@
+package edu.miami.cs.ross_pierson.mondrianmapmaker;
+
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.navigation.NavHost;
+
+import android.os.Bundle;
+
+public class WelcomeActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_welcome);
+    }
+
+    public void myClickHandler(android.view.View button){
+
+    }
+}
